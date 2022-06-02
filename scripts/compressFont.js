44d8f7b7869c147ecb41894d7d2a89de90b8bd02
qianphong <qianphong@163.com>
2022-06-02 11:55:35 +0800

    chore: udpate

--- scripts/compressFont.js
+++ scripts/compressFont.js
@@ -1,6 +1,5 @@
 const Fontmin = require('fontmin')
-const words =
-  '荣誉奖项全国重点镇、全国特色小镇、国家卫生镇、全国环境优美镇、中国休闲服装名镇、全国小城镇建设示范镇、中国人居环境范例奖、全国发展改革试点小城镇、中国苏作红木家具名镇'
+const words = ''
 
 const fontmin = new Fontmin()
   .src('src/assets/fonts/source/*.ttf')
