da524aa7796cbc0c892f27259b9f74fbb291ae11
qianphong <qianphong@163.com>
2022-08-29 13:07:50 +0800

    chroe: update

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite/client" />
-/// <reference types="unplugin-icons/types/vue3" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
 
