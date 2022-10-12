3422ee7274d5da7717724193fa56657a1e59a83c
qianphong <44841842+qianphong@users.noreply.github.com>
2022-10-12 08:09:51 +0000

    Deploying to gh-pages from @ qianphong/vue-view-template@ef96083e18a6e83e58d5736500fe44c1286605f1 🚀

---
+++ assets/_...all_.d878c76c.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as _,e}from"./index.f5d29c66.js";const c={},s={class:"text-xl my-4"};function n(a,r){return o(),_("div",s,"\u9875\u9762\u4E0D\u5B58\u5728")}typeof e=="function"&&e(c);const l=t(c,[["render",n]]);export{l as default};
