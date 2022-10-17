9b80cd1a10ba0303f9f9d3a2656e2d968cbfce3d
qianphong <qianphong@163.com>
2022-10-17 15:01:11 +0800

    refactor: import `tailwind.css` in the scss file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 import generatedRouters from '~pages'
 
-import '@unocss/reset/tailwind.css'
 import '@/styles/main.scss'
 import 'uno.css'
 
