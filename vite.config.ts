67e23666175b135f4ae01b22c4e9ce578cba1662
qianphong <qianphong@163.com>
2022-10-17 14:59:54 +0800

    chore: change case

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
+import Vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -10,8 +10,8 @@ import Components from 'unplugin-vue-components/vite'
 export default defineConfig({
   base: '/vue-view-template/',
   plugins: [
-    vue(),
-    Unocss(),
+    Vue(),
+    UnoCSS(),
     Pages({ exclude: ['**/components/**', '**/**/*.ts'] }),
     Layouts({
       defaultLayout: 'default',
